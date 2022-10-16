d94137934f419b3c72529a77c07a6232d7ef6786
Mountian Driver <lsfreigenberg@outlook.com>
2022-10-16 16:44:09 -0600

    create route to about

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,10 +2,19 @@ const router = require('express').Router();
 
 router.get('/', async (req, res)=>{
     try{
-        res.render('homepage')
+        res.render('work');
     }catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
+router.get('/about', async (req, res)=>{
+    try {
+        res.render('about');
+    } catch (err){
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports= router;
