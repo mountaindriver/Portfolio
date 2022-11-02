e926edeb850a37432c05794db53d60c77a0feb87
Mountian Driver <lsfreigenberg@outlook.com>
2022-11-02 11:59:43 -0600

    Start React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Homepage from './componets/home.js'
+
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/componets/home.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+
+function home(){
+    return (
+        <div>
+            <h1>Lucas Freigenberg</h1>
+            <h2>Full Stack Developer</h2>
+            <p>(435)659-8934 
+                lsfreigenberg@outlook.com</p>
+        </div>
+    )
+};
+
+export default home;
