f56ec16b0df1667dc7a0164b3abb6732a452285f
Mountian Driver <lsfreigenberg@outlook.com>
2022-11-04 11:26:40 -0600

    Add links to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ export default function Header (){
         <footer>
             <p>Lucas Freigenberg</p>
             <p>Full Stack Developer</p>
-            <p>Github: @mountiandriver</p>
-            <p>Email: lsfreigenberg@outlook.com</p>
-            <p>Phone (435) 659-8934</p>
+            <p><a href='https://www.linkedin.com/in/lucas-freigenberg-539338134/'>Linkedin Profile</a></p>
+            <p><a href='https://github.com/mountaindriver'> Github@mountiandriver</a></p>
+            <p><a href='mailto:lsfreigenberg@outlook.com'>Email Me</a></p>
         </footer>
     );
 }
