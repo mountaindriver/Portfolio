9e4fe96ab0718a6887564e6ea196e8acf3004ffa
Mountian Driver <lsfreigenberg@outlook.com>
2022-11-09 19:36:09 -0700

    Improve image on about scrren

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,9 @@ export default function About() {
       <div className='aCard'>
         <h1>Hello, I'm A <span>Full Stack</span> Web Developer</h1>
         <p>I'm Lucas a freelance web designer from Utah. I love to build, problem solve and learn new technologies.  I currently work within the MERN Stack.</p>
-        <img className="" src='./assests/images/outdoorsyProfile.jpg' alt='Lucas Freigenberg Mountian Biking' width='400px' height='300px' />
+        <div className='aImg'>
+          <img src='./assests/images/outdoorsyProfile.jpg' alt='Lucas Freigenberg Mountian Biking' />
+        </div>
       </div>
     </section>
   );
