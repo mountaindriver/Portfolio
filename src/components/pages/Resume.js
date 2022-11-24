672280baaf5a87eb22ac41dd63fe726aecba9380
Mountian Driver <lsfreigenberg@outlook.com>
2022-11-23 18:35:39 -0700

    Updated resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -6,7 +6,7 @@ export default function Resume() {
     <section>
       <div className='aCard'>
         <h1>Thanks for checking out my <span>Portfolio</span> 😎</h1>
-        <a href="assests/resume/Lucas Freigenberg.pdf" download>
+        <a href="assests/resume/Lucas Freigenberg Resume.pdf" download>
           <h3>My Resume📝</h3></a>
       </div>
     </section>
